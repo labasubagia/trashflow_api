e2023679a11d6de2260ade825050511a883244d3
Laba Subagia <labasubagia22@gmail.com>
2022-01-07 22:09:42 +0800

    update user

--- controller/user.js
+++ controller/user.js
@@ -1,23 +1,46 @@
+const multer = require('multer');
+const authMiddleware = require('../middleware/auth');
+
 class UserController {
   constructor({ userService }) {
     this.userService = userService;
+    this.upload = multer({ storage: multer.memoryStorage() });
   }
 
-  async create(req, res, next) {
-    try {
-      const user = await this.userService.upsert({
-        ...req.body,
-        file: req.file,
-      });
-      return res.json({
-        success: true,
-        data: user,
-        message: 'save user success',
-      });
-    } catch (err) {
-      err.statusCode = 400;
-      return next(err);
-    }
+  upsert() {
+    return [this.upload.single('image'), this.upsertHandler()];
+  }
+
+  upsertHandler() {
+    return async (req, res, next) => {
+      try {
+        const user = await this.userService.upsert({
+          ...req.body,
+          file: req.file,
+        });
+        return res.json({
+          success: true,
+          data: user,
+          message: 'save user success',
+        });
+      } catch (err) {
+        err.statusCode = 400;
+        return next(err);
+      }
+    };
+  }
+
+  getProfile() {
+    return [
+      authMiddleware({ userModel: this.userService.userModel }),
+      this.getProfileHandler(),
+    ];
+  }
+
+  getProfileHandler() {
+    return async (req, res) => {
+      return res.json({ success: true, data: req.user, message: '' });
+    };
   }
 }
 
--- router/user.js
+++ router/user.js
@@ -1,11 +1,9 @@
 const { Router } = require('express');
-const multer = require('multer');
 const UserController = require('../controller/user');
-const authMiddleware = require('../middleware/auth');
 const User = require('../models/user');
 const UserService = require('../service/user');
 
-const upload = multer({ storage: multer.memoryStorage() });
+const userRouter = Router();
 
 const controller = new UserController({
   userService: new UserService({
@@ -13,14 +11,7 @@ const controller = new UserController({
   }),
 });
 
-const userRouter = Router();
-
-userRouter.post('/', upload.single('image'), (req, res, next) => {
-  return controller.create(req, res, next);
-});
-
-userRouter.get('/', authMiddleware({ userModel: User }), (req, res) => {
-  return res.json({ success: true, data: req.user, message: '' });
-});
+userRouter.post('/', ...controller.upsert());
+userRouter.get('/', ...controller.getProfile());
 
 module.exports = userRouter;
